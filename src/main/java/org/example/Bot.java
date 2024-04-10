c0d10d391e53049f2c46da1c191e8ac91749a185
ProninNA <pro100monster@gmail.com>
2024-04-10 09:49:33 +0300

    Рабочая версия

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,7 +1,13 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ForwardMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Objects;
+import java.util.logging.Level;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -20,7 +26,48 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
+        var msg = update.getMessage();
+        var user = msg.getChat();
+        var id = user.getId();
+        var userName = user.getUserName();
+        var firstName = user.getFirstName();
+        var lastName = user.getLastName();
 
+
+        forwardMessage(id,msg.getText(),msg.getMessageId());
+        System.out.println(update);
+        switch (msg.getText()){
+            case "/start":
+                if (Objects.isNull(userName)){
+                    sendMessage(id, "Привет, " + user.getFirstName() + "!");
+                }else {
+                    sendMessage(id, "Привет, " + user.getUserName() + "!");
+                };
+            break;
+            case "/help": sendMessage(id, user.getFirstName() + ", это мой первый бот на котором я отрабатываю азы Java.");
+            break;
+            default: sendMessage(id, "Введена не верная команда, попробуй /start или /help");
+        }
+    }
+
+    public void sendMessage (Long who, String msg){
+        SendMessage sm = new SendMessage();
+        sm.setChatId(who.toString());
+        sm.setText(msg);
+        try{
+            execute(sm);
+        }catch (TelegramApiException e) {
+        }
+    }
+
+    public void forwardMessage (Long who, String msg, Integer mId){
+        ForwardMessage fm = new ForwardMessage();
+        fm.setChatId("937081213");      //мой чат с ботом
+        fm.setFromChatId(who.toString());              //
+        fm.setMessageId(mId);              //
+        try{
+            execute(fm);
+        }catch (TelegramApiException e) {
+        }
     }
 }
