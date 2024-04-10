511717f96363a1146aa1b2a67b26088725478856
ProninNA <pro100monster@gmail.com>
2024-04-10 09:14:02 +0300

    First start

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
 public class Bot extends TelegramLongPollingBot {
+
+    final private String BOT_TOKEN = "6710916054:AAGphNqeNuzdMiF0LlCH657FdddiuloGfJ4";
+    final private String BOT_NAME = "MyFirstBot";
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update);
+
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
 
-public class Main {
-    public static void main(String[] args) {
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new Bot());
     }
 }
