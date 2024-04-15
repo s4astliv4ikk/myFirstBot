ff728f68808bf9eb92f19be54f1aae826e6f16ca
ProninNA_work <pro100monster@gmai.com>
2024-04-15 17:02:35 +0300

    New bot with buttons

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -6,9 +6,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.Objects;
-import java.util.logging.Level;
-
 public class Bot extends TelegramLongPollingBot {
 
     final private String BOT_TOKEN = "6710916054:AAGphNqeNuzdMiF0LlCH657FdddiuloGfJ4";
@@ -26,27 +23,52 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var msg = update.getMessage();
-        var user = msg.getChat();
-        var id = user.getId();
-        var userName = user.getUserName();
-        var firstName = user.getFirstName();
-        var lastName = user.getLastName();
+//        var msg = update.getMessage();
+//        var user = msg.getChat();
+//        var id = msg.getChatId();
+//        var userName = user.getUserName();
+//        var firstName = user.getFirstName();
+//        var lastName = user.getLastName();
 
 
-        forwardMessage(id,msg.getText(),msg.getMessageId()); //персылка сообщений в мой час с ботом
-        System.out.println(update);                          //вывод данных в консоль
-        switch (msg.getText()){                              //получение команды
-            case "/start":
-                if (Objects.isNull(userName)){
-                    sendMessage(id, "Привет, " + user.getFirstName() + "!");
-                }else {
-                    sendMessage(id, "Привет, " + user.getUserName() + "!");
-                };
+
+//        switch (msg.getText()){                              //получение команды
+//            case "/start":
+//                if (Objects.isNull(userName)){
+//                    sendMessage(id, "Привет, " + user.getFirstName() + "!");
+//                }else {
+//                    sendMessage(id, "Привет, " + user.getUserName() + "!");
+//                };
+//                break;
+//            case "/help": sendMessage(id, user.getFirstName() + ", это мой первый бот на котором я отрабатываю азы Java.");
+//                break;
+//            default: sendMessage(id, "Введена неверная команда, попробуй /start или /help");
+//        }
+
+        if (update.hasMessage() && update.getMessage().hasText()){
+            var msg = update.getMessage();
+            var user = msg.getChat();
+            var id = msg.getChatId();
+            forwardMessage(id,msg.getText(),msg.getMessageId()); //персылка сообщений в мой час с ботом
+            System.out.println(update);                          //вывод данных в консоль
+            switch (msg.getText()){
+                case "/start":
+                    try {
+                        execute(Buttons.button(id));
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    };
                 break;
-            case "/help": sendMessage(id, user.getFirstName() + ", это мой первый бот на котором я отрабатываю азы Java.");
+                default: sendMessage(id, "Введена неверная команда, попробуйте /start");
+            }
+        }else if (update.hasCallbackQuery()){
+            String callData = update.getCallbackQuery().getData();
+            Long cId = update.getCallbackQuery().getMessage().getChatId();
+            switch (callData){
+                case "Hi": sendMessage(cId, "Hi, " + update.getCallbackQuery().getFrom().getFirstName());
                 break;
-            default: sendMessage(id, "Введена не верная команда, попробуй /start или /help");
+                case "Help": sendMessage(cId, update.getCallbackQuery().getFrom().getFirstName() + ", это мой первый бот на котором я отрабатываю азы Java.");
+            }
         }
     }
 
